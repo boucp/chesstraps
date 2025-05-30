9e8b110ad20f010c5b518733a2e44bf710304c60
boucp <130845503+boucp@users.noreply.github.com>
2025-05-30 18:06:41 +0600

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,6 @@ function setupBoard(pgn) {
 function drawBoard() {
   boardDiv.innerHTML = "";
   const position = game.board();
-   if(isFlipped) {
   for (let r = 7; r >=0;r--) {
     for (let c = 7;c >=0;c--) {
       const square = document.createElement("div");
@@ -53,26 +52,7 @@ function drawBoard() {
     }
   }
 }
-  else {
-    for (let r = 0; r <8;r++) {
-    for (let c = 7;c >=0;c--) {
-      const square = document.createElement("div");
-      square.className = "square " + ((r + c) % 2 === 0 ? "light" : "dark");
-
-      const piece = position[r][c]; // This flips vertical access correctly
-      if (piece) {
-        const img = document.createElement("img");
-        const code = piece.color + piece.type.toUpperCase();
-        img.src = pieces[code];
-        square.appendChild(img);
-      }
-
-      boardDiv.appendChild(square);
-    }
-  }
-}
-}
-
+  
 
 
 function nextMove() {
